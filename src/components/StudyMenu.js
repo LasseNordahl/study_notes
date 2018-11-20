be792e70378901346a573240f7bdaef83fb0e770
Lasse Nordahl <lasseanordahl@gmail.com>
2018-11-20 12:03:13 -0800

    youtube now loads correctly
    update

--- src/components/StudyMenu.js
+++ src/components/StudyMenu.js
@@ -1,16 +1,20 @@
 import React, { Component } from "react";
 import "../App.css";
 
-import Split from './Split.js'
-
 import Card from "@material-ui/core/Card";
 import Button from "@material-ui/core/Button";
 import Description from "@material-ui/icons/Description";
 import PlayArrow from "@material-ui/icons/PlayArrow";
+import Settings from "@material-ui/icons/Settings";
+
+const config = require('../config.js')
+
+console.log(config.default.API)
+
 
 var menu_card = {
     width: '100%',
-    height: '15vh'
+    // height: '15vh'
 };
 
 var button_margins = {
@@ -34,25 +38,65 @@ var button_config = {
         color: 'secondary',
         icon: <PlayArrow/>,
         type: 'extendedFab'
+    },
+    'Settings' : {
+        name: 'Settings',
+        color: 'default',
+        icon: <Settings/>,
+        type: 'fab'
     }
 }
 
-var test_buttons = ['Youtube', 'Quizlet']
+var test_buttons = ['Youtube', 'Quizlet', 'Settings']
 
 class StudyMenu extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            youtube_videos: [1, 2, 3]
+        }
+
+        this.load_youtube_videos = this.load_youtube_videos.bind(this)
+    }
+
+    componentDidMount() {
+        let component = this
+        fetch(config.default.API + 'getVideos?keyword=LasseNordahl')
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(response) {
+            component.setState({
+                youtube_videos: response
+            })
+            console.log(response);
+        });
+    }
+
+    load_youtube_videos() {
+        console.log('loading youtube videos')
+        console.log(process.env.REACT_APP_STAGE)
+    }
+
     render() {
         return (
             <div>
                 <Card style={menu_card}>
                     <ButtonMargin>
-                        {test_buttons.map(button => (
-                            <Button style={menu_button} color={button_config[button].color} variant={button_config[button].type}>
+                        {test_buttons.map((button, i) => (
+                            <Button style={menu_button} 
+                                    color={button_config[button].color} 
+                                    variant={button_config[button].type}
+                                    onClick={this.load_youtube_videos}
+                                    key={i}>
                                 {button_config[button].icon}
-                                {button}
+                                {button_config[button].type === 'extendedFab' ? button : ''}
                             </Button>
                         ))}
                     </ButtonMargin>
                 </Card>
+                <pre>{JSON.stringify(this.state.youtube_videos)}</pre>
             </div>
         );
     }
---
+++ src/config.js
@@ -0,0 +1,27 @@
+const dev = {
+    API: 'http://localhost:8080/',
+};
+  
+const prod = {
+    s3: {
+        BUCKET: "YOUR_PROD_S3_UPLOADS_BUCKET_NAME"
+    },
+    apiGateway: {
+        REGION: "YOUR_PROD_API_GATEWAY_REGION",
+        URL: "YOUR_PROD_API_GATEWAY_URL"
+    },
+    cognito: {
+        REGION: "YOUR_PROD_COGNITO_REGION",
+        USER_POOL_ID: "YOUR_PROD_COGNITO_USER_POOL_ID",
+        APP_CLIENT_ID: "YOUR_PROD_COGNITO_APP_CLIENT_ID",
+        IDENTITY_POOL_ID: "YOUR_PROD_IDENTITY_POOL_ID"
+    }
+};
+  
+const config = process.env.REACT_APP_STAGE === 'production' ? prod : dev;
+  
+export default {
+    // Add common config values here
+    MAX_ATTACHMENT_SIZE: 5000000,
+    ...config
+};
